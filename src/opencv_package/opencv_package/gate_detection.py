cb3a507bce89c78d0e942fd6020861b1934e003a
Sandra <svekho@utu.fi>
2025-04-07 17:00:38 +0300

    dilation

--- src/opencv_package/opencv_package/gate_detection.py
+++ src/opencv_package/opencv_package/gate_detection.py
@@ -28,7 +28,6 @@ class GateDetector(Node):
         cv2.imshow("Received frame", frame)
         cv2.waitKey(1)
 
-        ### TEST CODE STARTS ###
         frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         lower_y = np.array([35, 40, 40])
         upper_y = np.array([80, 255, 255])
@@ -36,41 +35,46 @@ class GateDetector(Node):
         mask = cv2.inRange(frame_hsv, lower_y, upper_y)
         cv2.imshow("Green values", mask)
         cv2.waitKey(1)
-        ### TEST CODE ENDS ###
 
         # Preprocess the image with conversion to grayscale and gaussian blur to reduce noice in the image
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray_with_green_mask = cv2.bitwise_or(gray, mask)
 
-        cv2.imshow("Gray without mask", gray)
-        cv2.waitKey(1)
         cv2.imshow("Gray with mask", gray_with_green_mask)
         cv2.waitKey(1)
+
         blur = cv2.GaussianBlur(gray_with_green_mask, (15, 15), 0)
         cv2.imshow("Blurred", blur)
         cv2.waitKey(1)
-
+        
         # Detect edges, highlighting the significant transitions in pixel intensity (usually correspons to object boundaries)
         # ADJUST THE THRESHOLDS TO CONTROL THE SENSITIVITY OF EDGE DETECTION
-        edges = cv2.Canny(blur, 100, 255)
+        edges = cv2.Canny(blur, 100, 150)
+
+        kernel = np.ones((7,7),np.uint8)
+        dilation = cv2.dilate(edges,kernel,iterations = 1)
 
         # Find countours in the edge-detected image (adjust the second argument for other contour retrieval modes)
-        contours, _ = cv2.findContours(edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        contours, _ = cv2.findContours(dilation.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         
         ### TEST CODE STARTS ###
         output = frame.copy()
         cv2.drawContours(output, contours, -1, (0,255,0), 3)
         cv2.imshow("Contoured frame", output)
         cv2.waitKey(1)
+
         selected_contours = []
+        biggest_area = 0
         ### TEST CODE ENDS ###
 
         for contour in contours:
             ### TEST CODE STARTS ###
             area = cv2.contourArea(contour)
 
-            if area > 1000:
+            if area > 20000:
                 selected_contours.append(contour)
+                if area > biggest_area:
+                    biggest_area = area
             ### TEST CODE ENDS ###
             
             # Approximating contours: simplify the contour while preserving its core structure
@@ -95,6 +99,7 @@ class GateDetector(Node):
         ### TEST CODE STARTS ###
         output2 = frame.copy()
         print(len(selected_contours))
+        print(biggest_area)
         cv2.drawContours(output2, selected_contours, -1, (255,0,0), 3)
         cv2.imshow("Selected contour frame", output2)
         cv2.waitKey(1)
