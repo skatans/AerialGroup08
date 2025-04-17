2c1f873457443285a846132dfca28e5bb32b1c5f
Tuisku <tuiskut@proton.me>
2025-04-17 16:38:24 +0300

    beter red filter

--- src/opencv_package/opencv_package/gate_detection.py
+++ src/opencv_package/opencv_package/gate_detection.py
@@ -37,21 +37,25 @@ class GateDetector(Node):
             print(f"Service call failed: {e}")
 
     def listener_callback(self, data):
-        self.get_logger().info('Receiving video frame')
+        #self.get_logger().info('Receiving video frame')
         frame = self.br.imgmsg_to_cv2(data, 'bgr8')
 
         area = detect_stop(frame)
-        print(area)
+        #print(area)
         if area > 0:
             while not self.client.wait_for_service(timeout_sec=1.0):
                 print('Service not available, waiting...')
 
             request = TelloAction.Request()
-            if area > 5000:
+            if area == 1:
+                self.get_logger().info('Takeoff command')
                 request.cmd = 'takeoff'
-            else:
+            elif area > 5000:
+                self.get_logger().info('Land command')
                 request.cmd = 'land'
-            print(request.cmd)
+            else:
+                self.get_logger().info('Land command')
+                request.cmd = 'land'
 
             future = self.client.call_async(request)
 
@@ -68,21 +72,36 @@ def detect_stop(image):
     blur = cv2.GaussianBlur(frame,(15,15),0)
     cv2.waitKey(1)
 
+    # yellow mask
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_yellow = np.array([10, 80, 80])
+    upper_yellow = np.array([50, 255, 255])
+    yellow_mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+
+    # Apply the yellow mask to the image
+    yellow_result = cv2.bitwise_and(frame, frame, mask=yellow_mask)
+
+    # Check if more than 75% of the image is yellow
+    white_pixels = cv2.countNonZero(yellow_mask)
+    total_pixels = yellow_mask.shape[0] * yellow_mask.shape[1]
+    white_ratio = white_pixels / total_pixels
+
+    if white_ratio > 0.75:
+        return 1
+
     # red mask
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     lower_red1 = np.array([0, 50, 50])
     upper_red1 = np.array([10, 255, 255])
-    lower_red2 = np.array([170, 50, 50])
+    lower_red2 = np.array([170, 150, 50])
     upper_red2 = np.array([180, 255, 255])
 
     mask_red1 = cv2.inRange(hsv, lower_red1, upper_red1)
     mask_red2 = cv2.inRange(hsv, lower_red2, upper_red2)
-    red_mask = cv2.bitwise_or(mask_red1, mask_red2)
+    red_mask = mask_red2
 
     # Apply the red mask to the image
     red_result = cv2.bitwise_and(frame, frame, mask=red_mask)
-    cv2.imshow("Red Mask Applied", red_result)
-    cv2.waitKey(1)
 
     red_edges = cv2.Canny(red_result, 50, 150)
     # Find contours in the edge-detected image
@@ -97,6 +116,8 @@ def detect_stop(image):
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
             cv2.putText(frame, "Big Red Area", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
+            cv2.imshow("Red Mask Applied", red_result)
+            cv2.waitKey(1)
             return area
     return 0
 
@@ -163,8 +184,8 @@ def detect_gate(image):
 
     # Display the combined mask
     res = cv2.bitwise_and(frame,frame, mask= combined_mask)
-    cv2.imshow("Combined Mask", combined_mask)
-    cv2.waitKey(1)
+    #cv2.imshow("Combined Mask", combined_mask)
+    #cv2.waitKey(1)
 
     res = combined_mask
     # Detect edges using Canny edge detection
@@ -254,7 +275,7 @@ def detect_gate(image):
         cv2.putText(frame, f"({center[0]}, {center[1]})", (center[0] + 5, center[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 0, 0), 1)
 
     #frame = res
-    cv2.imshow("Shape Detection", frame)
+    #cv2.imshow("Shape Detection", frame)
 
 
 def main(args=None):
