b96e2b8bcd6456bdd190ea57b52cbcac89ca18ec
Sandra Ekholm <42974706+svekho@users.noreply.github.com>
2025-04-24 19:28:02 +0300

    Update gate_detection.py
    test

--- src/opencv_package/opencv_package/gate_detection.py
+++ src/opencv_package/opencv_package/gate_detection.py
@@ -9,6 +9,8 @@ import cv2
 import numpy as np
 from tello_msgs.srv import TelloAction
 
+import torch
+
 import time
 
 flying = False
@@ -33,6 +35,8 @@ class GateDetector(Node):
             self.listener_callback,
             qos_profile=qos_profile)
         self.br = CvBridge()
+        #self.model = torch.hub.load('../ultralytics', 'custom', source='local', path='../ultralytics/weights/bests640e75n1010.pt', force_reload=True)
+        #self.model.eval()
 
     def service_response_callback(self, future):
         try:
@@ -78,156 +82,54 @@ class GateDetector(Node):
         frame = image
         image_height = frame.shape[0]
         image_width = frame.shape[1]
-        blur = cv2.GaussianBlur(frame,(15,15),0)
-        cv2.waitKey(1)
-
-        '''FILTERING'''
-        # green mask
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        lower_green = np.array([60, 80, 20])
-        upper_green = np.array([85, 255, 240])
-        mask = cv2.inRange(hsv, lower_green, upper_green)
-
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
-        morph = mask
-        morph = cv2.erode(morph,kernel,iterations = 2)
-        morph = cv2.dilate(morph,kernel,iterations = 3)
-
-        mask = morph
-    
-        # Check the lightness of the image
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        lightness = np.mean(hsv[:, :, 2])  # V channel represents lightness
-        #print("Lightness:", lightness)
-
-        # Choose the plywood array based on lightness
-        if lightness > 180:
-            plywood = plywood_light
-        elif lightness > 120:
-            plywood = plywood_mid
-        else:
-            plywood = plywood_dark
-
-        # plywood mask using HSV
-        hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-        lower_plywood_hsv = np.array([plywood[0][0], plywood[1][0], plywood[2][0]])
-        upper_plywood_hsv = np.array([plywood[0][1], plywood[1][1], plywood[2][1]])
-        plywood_mask_hsv = cv2.inRange(hsv, lower_plywood_hsv, upper_plywood_hsv)
-
-        morph = plywood_mask_hsv
-        morph = cv2.erode(morph, kernel, iterations=1)
-        morph = cv2.dilate(morph, kernel, iterations=3)
-
-        plywood_mask_hsv = morph
-
-        # white mask
-        lower_white = np.array([120, 0, 150])
-        upper_white = np.array([160, 40, 200])
-        mask_white = cv2.inRange(hsv, lower_white, upper_white)
-        morph = mask_white
-        morph = cv2.erode(morph,kernel,iterations = 1)
-        morph = cv2.dilate(morph,kernel,iterations = 10)
-        morph = cv2.erode(morph,kernel,iterations = 5)
-        mask_white = morph
-
-        # combined mask
-        combined_mask = cv2.bitwise_or(mask, plywood_mask_hsv)
-        #combined_mask = cv2.bitwise_or(combined_mask, mask_white)
-        morph = cv2.dilate(combined_mask, kernel, iterations=2)
-        combined_mask = morph
-
-        # Display the combined mask
-        res = cv2.bitwise_and(frame,frame, mask= combined_mask)
-        #cv2.imshow("Combined Mask", combined_mask)
-        #cv2.waitKey(1)
-
-        res = combined_mask
-        # Detect edges using Canny edge detection
-        edges = cv2.Canny(res, 50, 150)
-        # Find contours in the edge-detected image
-        contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
-        # Find the largest circle and midpoints of all circles
-        largest_circle = None
-        largest_radius = 0
-        circle_midpoints = []
-
-        # Draw contours on the original frame
-        for contour in contours:
-            continue
-            area = cv2.contourArea(contour)
-            #if area < 3000:
-            #    continue
-
-            # Fit a minimum enclosing circle around the contour
-            (x, y), radius = cv2.minEnclosingCircle(contour)
-            center = (int(x), int(y))
-            radius = int(radius)
-
-            if radius < 50:
-                continue
-
-            # Store the midpoint and dimensions of the circle
-            circle_midpoints.append((center, radius))
-
-            # Check if this is the largest circle
-            if radius > largest_radius:
-                largest_radius = radius
-                largest_circle = (center, radius)
-
-
-            # Approximate the contour to reduce the number of points
-            epsilon = 0.02 * cv2.arcLength(contour, True)
-            approx = cv2.approxPolyDP(contour, epsilon, True)
-            # Check the number of vertices in the approximated contour
-            vertices = len(approx)
-            if vertices == 4:
-                shape = "Square/Rectangle"
-            elif vertices > 4:
-                shape = "Circle"
-            else:
-                shape = "Other"
-
-            # Get the width and height of the bounding rectangle
-            x, y, w, h = cv2.boundingRect(contour)
-            width = w
-            height = h
-            # Calculate the aspect ratio
-            aspect_ratio = float(width) / height
-            print("Aspect Ratio:", aspect_ratio)
-            # Check if the aspect ratio is within a certain range
-            if aspect_ratio < 0.5 or aspect_ratio > 2:
-                continue
-            elif aspect_ratio < 0.9:
-                cv2.putText(frame, "danger", (x, y + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
         
-            # Draw the contour and label the shape
-            cv2.drawContours(frame, [approx], -1, (0, 255, 0), 2)
-            x, y, w, h = cv2.boundingRect(approx)
-            cv2.putText(frame, shape, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-            # Draw the bounding rectangle
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            '''
-            (x,y),radius = cv2.minEnclosingCircle(contour)
-            center = (int(x),int(y))
-            radius = int(radius)
-            cv2.circle(frame,center,radius,(0,255,0),2)
-            ellipse = cv2.fitEllipse(contour)
-            cv2.ellipse(frame,ellipse,(0,255,0),2)
-            '''
+        #results = self.model(frame)
+        results = []
+        detections = results.xyxy[0]
+
+        #draw result
+        for *box, conf, cls in detections:
+            x1, y1, x2, y2 = map(int, box)
+            label = [] #self.model.names[int(cls)]
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
+            cv2.putText(frame, f'{label} {conf:.2f}', (x1, y1 - 10),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 1)
+    
+        largest_gate = None
+        largest_area = 0
+
+        # Loop through detections and find the largest gate
+        for *box, conf, cls in detections:
+            label = [] #self.model.names[int(cls)]
+
+
+            x1, y1, x2, y2 = map(int, box)
+            width = x2 - x1
+            height = y2 - y1
+            area = width * height
+            # Check if this gate is larger than the previous largest gate
+            if area > largest_area:
+                largest_area = area
+                largest_gate = (x1, y1, x2, y2, largest_area)
+
+        if largest_gate:
+            x1, y1, x2, y2, area = largest_gate
+            # Draw a red dot in the center of the largest gate
+            center_x_gate = (x1 + x2) // 2
+            center_y_gate = (y1 + y2) // 2
+            cv2.circle(frame, (center_x_gate, center_y_gate), 5, (0, 0, 255), -1)  #
 
         # Draw the largest circle
-        if largest_circle:
-            center, radius = largest_circle
-            cv2.circle(frame, center, radius, (0, 255, 255), 2)
-            cv2.putText(frame, f"Radius: {radius}", (center[0] - 40, center[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+        if largest_gate:
+            #center, radius = largest_gate
+            #cv2.circle(frame, center, radius, (0, 255, 255), 2)
+            #cv2.putText(frame, f"Radius: {radius}", (center[0] - 40, center[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
             # Align the gate
-            image_center_height = image_height / 2
+            image_center_height = image_height / 3
             image_center_width = image_width / 2
 
-            error_height = center[1] - image_center_height
-            error_width = center[0] - image_center_width
+            error_height = center_y_gate - image_center_height
+            error_width = center_x_gate - image_center_width
 
             height_threshold = image_height / 10
             width_threshold = image_width / 10
@@ -259,7 +161,7 @@ class GateDetector(Node):
             if aligned == True:
                 msg = String()
                 # If the radius of the gate is large enough, move a lot forward
-                if radius*2 > 0.9*image_height:
+                if (x2-x1) > 0.9*image_height:
                     msg.data = "forwardlong"
                     self.publication.publish(msg)
                     time.sleep(2.5) # stabilization time
@@ -272,9 +174,9 @@ class GateDetector(Node):
         
 
         # Draw all circle midpoints
-        for center, radius in circle_midpoints:
-            cv2.circle(frame, center, 3, (255, 0, 0), -1)
-            cv2.putText(frame, f"({center[0]}, {center[1]})", (center[0] + 5, center[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 0, 0), 1)
+        #for center, radius in circle_midpoints:
+        #    cv2.circle(frame, center, 3, (255, 0, 0), -1)
+        #    cv2.putText(frame, f"({center[0]}, {center[1]})", (center[0] + 5, center[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 0, 0), 1)
 
         #frame = res
         #cv2.imshow("Shape Detection", frame)
